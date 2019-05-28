8b1022465877ac47f3b8b8ff45c364e1cb8efe8f
qq240814476 <240814476@qq.com>
2019-05-28 21:10:07 +0800

    第一天

--- Day01-15/Day01/code/hello.py
+++ Day01-15/Day01/code/hello.py
@@ -9,9 +9,9 @@ Date: 2018-02-26
 请将该文件命名为hello.py并在终端中通过下面的命令运行它
 python hello.py
 """
-
+'''
 print('hello, world!')
 # print("你好,世界！")
-print('你好', '世界')
-print('hello', 'world', sep=', ', end='!')
-print('goodbye, world', end='!\n')
+'''
+
+print('这是一个抢票小程序','遇到问题联系作者qq:240814476', sep=',',end='!\n')
