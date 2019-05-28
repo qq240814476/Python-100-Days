27c24cffc6773a7e81ee8f0a51e2e6863e46e09e
qq240814476 <240814476@qq.com>
2019-05-28 21:17:24 +0800

    day one

---
+++ Day01-15/Day01/code/draw.py
@@ -0,0 +1,12 @@
+import turtle
+
+turtle.pensize(10)
+turtle.pencolor('blue')
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.mainloop()
